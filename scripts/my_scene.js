0e9de0fb68e5592c1f14a1b93fb9f3a4794714fd
kudou <h.kudou.sys22@morijyobi.ac.jp>
2023-11-28 14:49:16 +0900

    演習1-2

--- scripts/my_scene.js
+++ scripts/my_scene.js
@@ -11,15 +11,15 @@ class MyScene extends Phaser.Scene {
     // シーンの事前読み込み処理
     preload() {
          // 画像の読み込み(使用する時の名前, パス)
-        this.load.image('sky', 'assets/sky.png');
-        this.load.image('alien1', 'assets/alien1.png');
+        this.load.image('background', 'assets/background.png');
+        this.load.image('taro', 'assets/taro.png');
     }
 
     // シーン初期化処理
     create() {
          // 単体画像をシーンに追加(X座標,Y座標,画像名)
-        this.add.image(D_WIDTH/2, D_HEIGHT/2, 'sky');
-        this.add.image(D_WIDTH/2, D_HEIGHT/2, 'alien1');
+        this.add.image(D_WIDTH/2, D_HEIGHT/2, 'background');
+        this.add.image(D_WIDTH/2, D_HEIGHT/2, 'taro');
         this.text = this.add.text(10, 10, 'Scene 1').setFontSize(32).setColor('#ff0');
     }
     
